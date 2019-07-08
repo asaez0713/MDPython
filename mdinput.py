2000a936839496a23dcc53876f375df0e05e6e9e
asaez0713 <andresjsaez@gmail.com>
2019-07-08 15:46:02 -0400

    Add files via upload

---
+++ mdinput.py
@@ -0,0 +1,197 @@
+# import routines for MDPython
+
+import re
+import numpy as np
+
+global natoms
+global atypes
+global nbonds
+global tbonds
+global box
+
+box = np.zeros(3)
+
+re_dict_data = {
+        'natoms': re.compile(r'(?P<natoms>\d+) atoms\n'),
+        'atypes': re.compile(r'(?P<atypes>\d+) atom types\n'),
+        'nbonds': re.compile(r'(?P<nbonds>\d+) bonds\n'),
+        'tbonds': re.compile(r'(?P<tbonds>\d+) bond types\n'),
+        'box_x': re.compile(r'(?P<box_x>[\d-]+ [\d-]+) xlo xhi\n'),
+        'box_y': re.compile(r'(?P<box_y>[\d-]+ [\d-]+) ylo yhi\n'),
+        'box_z': re.compile(r'(?P<box_z>[\d-]+ [\d-]+) zlo zhi\n')
+        }
+
+def parse_line(line,d):
+    for key, rx in d.items():
+        match = rx.search(line)
+        if match:
+            return key, match
+
+    return None, None
+
+def readinvals(datafile):
+    with open(datafile,'r') as f:
+        data = []
+        line = f.readline()
+        while line:
+            key, match = parse_line(line,re_dict_data)
+            for item in re_dict_data:
+                if key == item:
+                    val = match.group(item)
+                    val = val.split()
+                    if len(val) > 1:
+                        data.append(float(val[1])-float(val[0]))
+                    else:
+                        data.append(int(val[0]))
+            line = f.readline()
+    return data
+
+re_dict_arrays = {
+        'masses': re.compile(r'Masses\n'),
+        'atoms': re.compile(r'Atoms'),
+        'vels': re.compile(r'Velocities\n'),
+        'bonds': re.compile(r'Bonds\n')
+        }
+
+def make_arrays(datafile):
+    global natoms, atypes, nbonds, tbonds, box
+    
+    natoms, atypes, nbonds, tbonds, box[0], box[1], box[2] = readinvals(datafile) 
+    
+    with open(datafile,'r') as f:
+        mass = []
+        aatype = []
+        pos = []
+        vel = []
+        masses = []
+        bonds = []
+        
+        line = f.readline()
+        while line:
+            key, match = parse_line(line,re_dict_arrays)
+            count = 1
+            if key == 'masses':
+                line = f.readline()
+                while len(line.split()) == 0:
+                    line = f.readline()
+                for i in range(atypes):
+                    words = line.split()
+                    if int(words[0]) != count:
+                        print('Error while assigning masses')
+                        print('Expecting',count,'got',words[0])
+                        exit(1)
+                    m = float(words[1])
+                    mass.append(m)
+                    count += 1
+                    line = f.readline()
+                print('Assigned masses')
+            if key == 'atoms':
+                line = f.readline()
+                while len(line.split()) == 0:
+                    line = f.readline()
+                for i in range(natoms):
+                    words = line.split()
+                    if int(words[0]) != count:
+                        print('Error while assigning atoms')
+                        print('Expecting',count,'got',line.split()[0])
+                        exit(1)
+                    atype = int(words[1])
+                    aatype.append(atype)
+                    pos.append([float(words[j + 4]) for j in range(3)])
+                    masses.append([mass[atype - 1] for j in range(3)])
+                    count += 1
+                    line = f.readline()
+                print('Assigned atom types and positions')
+            if key == 'vels':
+                line = f.readline()
+                while len(line.split()) == 0:
+                    line = f.readline()
+                for i in range(natoms):
+                    words = line.split()
+                    if int(words[0]) != count:
+                        print('Error while assigning velocities')
+                        print('Expecting',count,'got',line.split()[0])
+                        exit(1)
+                    vel.append([float(words[j + 1]) for j in range(3)])
+                    count += 1
+                    line = f.readline()
+                print('Assigned velocities')
+            if key == 'bonds':
+                line = f.readline()
+                while len(line.split()) == 0:
+                    line = f.readline()
+                for i in range(nbonds):
+                    words = line.split()
+                    if int(words[0]) != count:
+                        print('Error while assigning velocities')
+                        print('Expecting',count,'got',line.split()[0])
+                        exit(1)
+                    bonds.append([int(words[j + 1])-1 for j in range(3)])
+                    count += 1
+                    line = f.readline()
+                print('Assigned bonds')
+            line = f.readline()
+    
+    if len(vel) == 0:
+        vel = np.zeros((natoms,3))
+        print('Velocities will start at 0')
+    if len(bonds) == 0:
+        print('No bonds found')
+
+    return np.array(mass), np.array(aatype), np.array(pos), np.array(vel), np.array(masses), np.array(bonds)
+
+re_dict_sysvals = {
+        'nsteps': re.compile(r'run (?P<nsteps>\d+)'),
+        'dt': re.compile(r'timestep (?P<dt>[\d.]+)'),
+        'initfile': re.compile(r'read_data (?P<initfile>[ a-z.]+)'),
+        'ithermo': re.compile(r'thermo (?P<ithermo>\d+)'),
+        'dump': re.compile(r'dump traj all xyz (?P<dump>\d+ [a-z.]+)'),
+        'bond_style': re.compile(r'bond_style (?P<bond_style>[a-z]+)'),
+        'logfile': re.compile(r'log (?P<logfile>[a-z.]+)'),
+        'inm': re.compile(r'inm (?P<inm>[a-z.]+ \d+)')
+        }
+
+def readsysvals(infile):
+    with open(infile,'r') as f:
+        data = []
+        bondcoeff = []
+        line = f.readline()
+        while line:
+            key, match = parse_line(line,re_dict_sysvals)
+            if key in ['nsteps','ithermo']:
+                data.append(int(match.group(key)))
+            if key == 'dt':
+                data.append(float(match.group(key)))
+            if key == 'initfile':
+                initfile = match.group(key).strip(' ')
+                data.append(initfile)
+                natoms, atypes, nbonds, tbonds, box[0], box[1], box[2] = readinvals(initfile) 
+            if key in ['logfile','bond_style']:
+                data.append(match.group(key).strip(' '))
+            if key == 'dump':
+                val = match.group(key).split()
+                data.append(int(val[0]))
+                data.append(val[1])
+            if key == 'inm':
+                val = match.group(key).split()
+                data.append(val[0])
+                data.append(int(val[1]))
+            if key == 'bond_style':
+                bond_styles = match.group(key)
+                if re.search('harmonic',bond_styles,flags=re.IGNORECASE):
+                    bond_style = 0
+                    for i in range(tbonds):
+                        line = f.readline()
+                        bondcoeff.append([float(line.split()[j+2]) for j in range(2)])
+                elif re.search('morse',bond_styles,flags=re.IGNORECASE):
+                    bond_style = 1
+                    for i in range(tbonds):
+                        line = f.readline()
+                        bondcoeff.append([float(line.split()[j+2]) for j in range(3)])
+                else:
+                    print('Unrecognized bond type')
+            line = f.readline()
+    
+    bondcoeff = np.array(bondcoeff)
+
+    return data, bond_style, bondcoeff
---
+++ mdlammps.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python3
+# run MD using lammps style input
+
+import sys
+import numpy
+import math
+import time
+
+#import mdglobal  # file with global variables
+import mdinput   # file with input routines
+import mdoutput  # file with output routines
+import mdbond    # file with bonding routines
+
+# assigned global variables
+global natoms       # number of atoms
+global atypes       # number of atom types
+global nbonds       # number of bonds
+global tbonds       # number of bond types
+global box          # box coordinates
+global pot          # potential components
+global mass         # masses of each type
+global masses       # broadcast of mass type to atoms
+global pos          # positions of each atom
+global vel          # velocities of each atom
+global acc          # acceleration of each atom
+global aatype       # array of atom types
+global bonds        # bonds (array with type, ibond, jbond)
+global hessian      # hessian matrix
+global abtype       # array of bond types
+global logfile      # file to output thermodata
+
+box = numpy.zeros(3)
+pot = numpy.zeros(6)
+
+#------------------------------------------------
+def zero_momentum(masses,vel): #zero the linear momentum
+    mom = masses*vel # get momentum
+    tmom = numpy.sum(mom,axis=0)/numpy.sum(masses,axis=0) #total mom/ma
+    vel -= tmom #zero out
+
+#-------------------------------------------------
+def readinit(datafile): # read lammps init data file
+
+    global natoms, atypes, nbonds, tbonds, box
+
+    # unpack or destructuring
+    natoms, atypes, nbonds, tbonds, box[0], box[1], box[2] = mdinput.readinvals(datafile) 
+    print("Natoms",natoms," Atypes",atypes," Bonds",nbonds," Btypes",tbonds)
+    print("Box",box)
+
+    # allocate arrays from data
+    global mass, aatype, pos, vel, acc, masses, bonds, hessian
+
+    acc = numpy.zeros((natoms,3))
+    hessian = numpy.zeros((3*natoms,3*natoms))
+
+    mass, aatype, pos, vel, masses, bonds = mdinput.make_arrays(datafile)
+
+#-------------------------------------------
+def readin(): # read lammps like infile
+
+    global nsteps, dt, initfile, ithermo, idump, dumpfile, bond_style, bondcoeff
+    global logfile, inmfile, inmo
+    global bondcoeff
+
+    # print lines
+    data, bond_style, bondcoeff = mdinput.readsysvals(sys.argv[1]) # read lammps in file
+    dt, initfile, bond_styles, idump, dumpfile, ithermo, logfile, inmfile, inmo, nsteps = data
+    print("dt, initfile, bond_styles, idump, dumpfile, ithermo, logfile, inmfile, inmo, nsteps",data)
+
+#-----------------------------------------------------------
+def force(): # get forces from potentials
+    global pot, nbonds, bonds, bondcoeff
+    global masses, pos, vel, acc
+
+    acc.fill(0) # zero out forces/acceration
+    # lj
+    pot[0] = 0
+    # bonds
+    pot[1] = mdbond.bond_force(bond_style,nbonds,bonds,bondcoeff,pos,acc)
+    # bend
+    pot[2] = 0
+    # torsion
+    pot[3] = 0
+
+    # change forces into accelerations
+    acc /= masses
+
+#-----------------------------------------------------------
+def step(): # velocity verlet (using 1/2 steps)
+    global pos, vel, acc, dt
+    # print istep,pos,vel,acc
+    vel += acc*dt/2.0
+    pos += vel*dt
+    force()
+    vel += acc*dt/2.0
+
+#-----------------------------------------------------------
+
+# read command line for input file
+if (len(sys.argv) != 2):  # error check that we have an input file
+    print("No input file? or wrong number of arguments")
+    exit(1)
+print (sys.argv)
+
+readin() # read infile
+readinit(initfile)
+
+# inital force and adjustments
+zero_momentum(masses,vel)  # zero the momentum
+force()
+teng = mdoutput.write_thermo(logfile,0,natoms,masses,pos,vel,pot)
+
+itime = 1
+tnow = time.time()
+ttime = tnow
+tol = 1e-8
+dump_vel = 5
+
+print("Running dynamics")
+
+eig_array = [] # empty array for the eigenvalues
+
+for istep in range(1,nsteps+1):
+
+    step() # take a step
+
+    if(istep%inmo==0): # get instantaneous normal modes
+        hessian = mdbond.inm(bond_style,nbonds,bonds,bondcoeff,pos,masses)
+
+        # print(hessian)
+        w,v = numpy.linalg.eig(hessian)
+        # remove lowest eigegvalues (translations of entire system)
+        idx = numpy.argmin(numpy.abs(w.real))
+        while abs(w[idx]) < tol:
+            print("Warning! Removing eigenvalue < tol",w[idx])
+            w = numpy.delete(w,idx)
+            idx = numpy.argmin(numpy.abs(w.real))
+        eig_array.append(w.real) # only get real part of array - imag do to round off error is small so we throw away.
+
+    if(istep%ithermo==0): # write out thermodynamic data
+        teng = mdoutput.write_thermo(logfile,istep,natoms,masses,pos,vel,pot)
+
+    if(istep%idump==0): # dump to xyz file so we can see this in lammps
+        mdoutput.write_dump(dumpfile,istep,natoms,pos,aatype)
+
+    if(istep%dump_vel==0):
+        mdoutput.write_dump_vel("vel.dat",istep*dt,natoms,vel)
+
+    if(itime < time.time()-tnow): # report where we are
+        print('step = {}/{} = {:.4f}%, teng = {:g}, time = {:g}'.format(istep,nsteps,istep/nsteps*100,teng,time.time()-ttime))
+        tnow = time.time()
+
+print('Done dynamics! total time = {:g} seconds'.format(time.time()-ttime))
+mdoutput.write_init("test.init",istep-1,natoms,atypes,nbonds,tbonds,box,mass,pos,vel,bonds,aatype)
+
+#Create histogram!
+nconf = len(eig_array)
+if(nconf==0):
+    print("No configurations calculated eigenvalues! thus NOT calculating historgram")
+else:
+    print("Creating Histogram with",len(eig_array),"configurations")
+    q1, q3 = numpy.percentile(eig_array, [25, 75])
+    iqr = q3 - q1
+
+    fd_width = 2*iqr/(nconf**(1/3))
+    fd = (numpy.amax(eig_array) - numpy.amin(eig_array))/fd_width
+    fd = int(fd) + 1
+
+    sturges = numpy.log2(nconf) + 1
+    sturges = int(sturges) + 1
+
+    bin_ct = max(fd,sturges)
+    histo,histedge = numpy.histogram(numpy.array(eig_array),bins=bin_ct,density=True)
+    histdat = numpy.zeros((histo.size,2))
+    for i in range(histo.size):
+        histdat[i][0] = (histedge[i]+histedge[i+1])/2
+        histdat[i][1] = histo[i]
+        #print(histo,histedge,histdat)
+    head = "Histogram of eigenvalues " + sys.argv[0] + " " + str(len(eig_array))
+    numpy.savetxt(inmfile,(histdat),header=head,fmt="%g")
+
+print("Done!")
+exit(0)
