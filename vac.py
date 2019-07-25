0f06b8f68cc474d67bdfbdf9451f481bf79d3b3e
asaez0713 <andresjsaez@gmail.com>
2019-07-25 01:35:48 -0400

    autocorrelation with Wiener-Khintchine theorem

--- vac.py
+++ vac.py
@@ -1,59 +1,56 @@
 #!/usr/bin/python3
 
-import numpy
+import numpy as np
 import sys
 import time
+from numpy.fft import *
 
-file = open("vel.dat","r")
+file = open(sys.argv[1],"r")
 
 lines = file.readlines()
 
-nwindow = 1024
 natoms = lines[0].split()[1]
 natoms = int(natoms)
 dt = float(lines[natoms+1].split()[3]) - float(lines[0].split()[3])
-print(natoms,nwindow,dt)
+print('natoms:',natoms)
+print('dt:',dt)
 
 nconf = len(lines)//(natoms+1)
 print("Number of configurations: ",nconf)
 
-data = numpy.zeros((nwindow,natoms,3))
-vac = numpy.zeros(nwindow)
-norm = numpy.zeros(nwindow)
+data = np.zeros((nconf,natoms,3))
+vac = np.zeros(nconf)
 
-for i in range(nwindow): # get initial data
+for i in range(nconf): # get initial data
     for j in range(natoms):
         line  = lines[i*(natoms+1)+j+1].split()
         data[i][j] = line
+
+norm = np.sum([np.linalg.norm(data[0][j])**2 for j in range(natoms)])
+
+data = np.transpose(data,(1,0,2))
+print(data.shape)
+
+def xcorr(x):
+    fftx = fft(x, n = 5, axis=1)
+    ret = ifft(fftx * fftx.conj(), axis=1)
+    ret = fftshift(ret, axes=1).real
+    ret = np.sum(ret,axis=1)
+    return ret
 
 itime = 10 
 tnow = time.time()
 ttime = tnow
 
-print("Correlating")
-for i in range(nconf-nwindow):
-    # Correlate
-    for k in range(nwindow):
-        for j in range(natoms):
-            vac[k] += numpy.dot(data[k][j],data[0][j])
-            norm[k] += numpy.dot(data[0][j],data[0][j])
-
-    # get next data window
-    for j in range(nwindow-1): # move array down
-        data[j] = data[j+1]
-    for j in range(natoms): # read data into end of array
-        data[-1][j] = lines[i*(natoms+1)+j+1].split()
-
-    if(itime < time.time()-tnow): # report where we are
-        print('conf = {}/{} = {:.4f}%, time = {:g}'.format(i,nconf,i/nconf*100,time.time()-ttime))
-        tnow = time.time()
-
-print('Done correlating. total time = {:g} seonds'.format(time.time()-ttime))
+for i in range(len(data)):
+    vac += xcorr(data[i])
 
 vac /= norm
 
+print('Done correlating. total time = {:g} seconds'.format(time.time()-ttime))
+
 print("Writing vac.dat")
-file = open("vac.dat","w")
-for i in range(nwindow):
+file = open("vac_new.dat","w")
+for i in range(nconf):
     line = str(dt*i) + " " + str(vac[i]) + "\n"
     file.write(line)
